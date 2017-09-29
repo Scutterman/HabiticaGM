af9e9954c5320edbbb371d3d5a605e423d2fc916
tom@cgfindies.co.uk <tom@cgfindies.co.uk>
2017-09-29 15:55:36 +0100

    Got code working for viewing all party members in the scroller, but need to get it to only run once the component has loaded.

--- scripts/habitica-fix.user.js
+++ scripts/habitica-fix.user.js
@@ -13,12 +13,35 @@
 /** Hooks **/
 $(function () {
     $(document).on('keypress', '.new-message-row textarea', send_chat_on_ctrl_enter);
+
+    // TODO:: work out how to hook into the app loaded callback so we are able to tweak the components.
+    /*$('#app').get(0).__vue__.$options.mounted.push(function(){
+        app_mounted();
+    });*/
 });
 
-
 /** Event callbacks **/
+function app_mounted(){
+    $('#app-header').parent().get(0).__vue__.$options.mounted.push(function(){
+        view_more_party_members();
+    });
+}
+
 function send_chat_on_ctrl_enter(e) {
     if (e.ctrlKey && e.which == 13) {
         $('.send-chat').click();
     }
 }
+
+function view_more_party_members() {
+    $('#app-header').parent().get(0).__vue__._computedWatchers.membersToShow.getter = function(){ return 1000; }
+    $('#app-header .party-members').css({width: 'auto', overflowX: 'scroll'});
+}
+
+/*
+This is how you dispatch an action from the store:
+let members = $('#app').get(0).__vue__.$store.dispatch('members:getGroupMembers', {
+        groupId: 'the-group-id',
+        includeAllPublicFields: true,
+      });
+*/
